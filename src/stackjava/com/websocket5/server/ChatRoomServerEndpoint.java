111ad1226d9398bed7a4528ada5f260d6347bbcb
hungnguyen2407 <14130047@st.hcmuaf.edu.vn>
2018-09-11 17:23:53 +0700

    Announce user had left

--- src/stackjava/com/websocket5/server/ChatRoomServerEndpoint.java
+++ src/stackjava/com/websocket5/server/ChatRoomServerEndpoint.java
@@ -12,32 +12,36 @@ public class ChatRoomServerEndpoint {
 
     static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
 
-	@OnOpen
-	public void handleOpen(Session session) {
-		users.add(session);
-	}
+    @OnOpen
+    public void handleOpen(Session session) {
+        users.add(session);
+    }
 
-	@OnMessage
-	public void handleMessage(String message, Session userSession) throws IOException {
-		String username = (String) userSession.getUserProperties().get("username");
-		if (username == null) {
-			userSession.getUserProperties().put("username", message);
-			userSession.getBasicRemote().sendText("System: you are connectd as " + message);
-		} else {
-			for (Session session : users) {
-				session.getBasicRemote().sendText(username + ": " + message);
-			}
-		}
-	}
+    @OnMessage
+    public void handleMessage(String message, Session userSession) throws IOException {
+        String username = (String) userSession.getUserProperties().get("username");
+        if (username == null) {
+            userSession.getUserProperties().put("username", message);
+            userSession.getBasicRemote().sendText("System: you are connectd as " + message);
+        } else {
+            for (Session session : users) {
+                session.getBasicRemote().sendText(username + ": " + message);
+            }
+        }
+    }
 
-	@OnClose
-	public void handleClose(Session session) {
-		users.remove(session);
-	}
+    @OnClose
+    public void handleClose(Session session) throws IOException {
+        String username = (String) session.getUserProperties().get("username");
+        users.remove(session);
+        for (Session s : users) {
+            s.getBasicRemote().sendText("System: " + username + " had left.");
+        }
+    }
 
-	@OnError
-	public void handleError(Throwable t) {
-		t.printStackTrace();
-	}
+    @OnError
+    public void handleError(Throwable t) {
+        t.printStackTrace();
+    }
 
 }
